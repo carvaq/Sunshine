9db71724ed25c1920aca5f93ef3a8e1d55e6a49d
carvaq <shoesforbreakfast@gmail.com>
2016-06-11 11:43:24 +0200

    Lesson 2

--- app/src/main/java/cvv/udacity/sunshine/MainActivity.java
+++ app/src/main/java/cvv/udacity/sunshine/MainActivity.java
@@ -1,19 +1,10 @@
 package cvv.udacity.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +16,5 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }}
 
-    private static class ForecastFragment extends Fragment {
-
-        public ForecastFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-            List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
-
-            ListView listView = (ListView) rootView.findViewById(R.id.list_forecast);
-
-            ArrayAdapter<String> weatherAdapter = new ArrayAdapter<>(getActivity(),
-                    R.layout.list_item_forcast, R.id.list_item_forecast_textview, weekForecast);
-            listView.setAdapter(weatherAdapter);
-            return rootView;
-        }
-    }
 }
 
