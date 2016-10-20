a51edc5e15a282ab6095d6fbe06e8d8bf2b14639
carvaq <shoesforbreakfast@gmail.com>
2016-10-20 11:44:20 +0200

    added material layout

--- app/src/main/java/cvv/udacity/sunshine/MainActivity.java
+++ app/src/main/java/cvv/udacity/sunshine/MainActivity.java
@@ -18,17 +18,14 @@ package cvv.udacity.sunshine;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-
 import cvv.udacity.sunshine.sync.SunshineSyncAdapter;
 
-public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
+public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
@@ -43,6 +40,10 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         mLocation = Utility.getPreferredLocation(this);
 
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         if (findViewById(R.id.weather_detail_container) != null) {
             // The detail container view will be present only in the large-screen layouts
             // (res/layout-sw600dp). If this view is present, then the activity should be
@@ -66,12 +67,6 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         forecastFragment.setUseTodayLayout(!mTwoPane);
 
         SunshineSyncAdapter.initializeSyncAdapter(this);
-
-        if (!checkPlayServices()) {
-            // This is where we could either prompt a user that they should install
-            // the latest version of Google Play Services, or add an error snackbar
-            // that some features won't be available.
-        }
     }
 
     @Override
@@ -135,26 +130,5 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
                     .setData(contentUri);
             startActivity(intent);
         }
-    }
-
-    /**
-     * Check the device to make sure it has the Google Play Services APK. If
-     * it doesn't, display a dialog that allows users to download the APK from
-     * the Google Play Store or enable it in the device's system settings.
-     */
-    private boolean checkPlayServices() {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (apiAvailability.isUserResolvableError(resultCode)) {
-                apiAvailability.getErrorDialog(this, resultCode,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(LOG_TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
     }
 }
--- app/src/main/java/cvv/udacity/sunshine/MessagingService.java
+++ app/src/main/java/cvv/udacity/sunshine/MessagingService.java
@@ -1,22 +1,64 @@
 package cvv.udacity.sunshine;
 
-import android.util.Log;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MessagingService extends FirebaseMessagingService {
-private static final String TAG = "MessagingService";
+    private static final String TAG = "MessagingService";
+    private static final String EXTRA_WEATHER = "weather";
+    private static final String EXTRA_LOCATION = "location";
+    public static final int NOTIFICATION_ID = 1;
+
     public MessagingService() {
     }
 
-  @  Override
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        try {
+            String message ="{\"weather\":\"Shit cold\",\"location\":\"Home\"}";
+            JSONObject jsonObject = new JSONObject(message);
+            String weather = jsonObject.getString(EXTRA_WEATHER);
+            String location = jsonObject.getString(EXTRA_LOCATION);
+            String alert =
+                    String.format(getString(R.string.gcm_weather_alert), weather, location);
+            sendNotification(alert);
+        } catch (JSONException e) {
+            // JSON parsing failed, so we just let this message go, since GCM is not one
+            // of our critical features.
+        }
+    }
+
+    private void sendNotification(String message) {
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent contentIntent =
+                PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        // Notifications using both a large and a small icon (which yours should!) need the large
+        // icon as a bitmap. So we need to create that here from the resource ID, and pass the
+        // object along in our notification builder. Generally, you want to use the app icon as the
+        // small icon, so that users understand what app is triggering this notification.
+        Bitmap largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.art_storm);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.art_clear)
+                        .setLargeIcon(largeIcon)
+                        .setContentTitle("Weather Alert!")
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                        .setContentText(message)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
